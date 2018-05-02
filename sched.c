79140b18813ac8b5c3c3cfeea79df96c8ca97c61
cubouyaka <cubouyaka@laposte.net>
2018-05-02 17:49:27 +0200

    debut de sched_init

--- sched.c
+++ sched.c
@@ -18,10 +18,47 @@ int sched_init(int nthreads, int qlen, taskfunc f, void *closure){
   //Creations des threads
   scheduler->threads=(pthread_t *)malloc(scheduler->nthreads *sizeof(pthread_t));
 
+  //Creation de la pile d'execution (lifo)
+  scheduler->lifo = (struct Lifo *) malloc(sizeof(struct Lifo));
+
+  //Empilation de la tache initiale sur la pile
+  empiler(scheduler->lifo,f,closure);
+
   return 0;
 }
 
 int sched_spawn(taskfunc f, void *closure, struct scheduler *s){
 
   return 0;
+}
+
+
+void empiler(struct Lifo * lifo, taskfunc f, void * closure){
+  struct Element * e = (Element *) malloc(sizeof(struct Element));
+  if (lifo == NULL || e == NULL)
+    exit(EXIT_FAILURE);
+  e->t = f;
+  e->closure = closure;
+  e->prec = lifo->dernier;
+  lifo->dernier = e;
+}
+
+Element *depiler(struct Lifo * lifo){
+    if (lifo == NULL)
+      exit(EXIT_FAILURE);
+
+    Element * e = lifo->dernier; //l'element a retourner
+    lifo->dernier = e->prec; //depiler l'element e
+
+    return e;
+}
+
+int tailleLifo(struct Lifo * lifo){
+  int c = 0;
+  struct Element * e = lifo->dernier;
+  while(e != NULL){
+    c++;
+    e = e->prec;
+  }
+  return c;
 }
--- sched.h
+++ sched.h
@@ -1,18 +1,22 @@
+#pragma once 
+#ifndef SCHED_H
+#define SCHED_H
+
+//#include "lifo.h"
+
 #include <unistd.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
 
-struct scheduler;
-
 typedef void (*taskfunc)(void*, struct scheduler *);
 
-
-struct scheduler {
+typedef struct scheduler {
   int nthreads; //nombre de threads
   int qlen; //nombres minimum de taches simultanees
   pthread_t * threads ;//tableau de threads
-};
+  struct Lifo * lifo; //la pile d'execution (lifo)
+} scheduler;
 
 static inline int sched_default_threads(){
     return sysconf(_SC_NPROCESSORS_ONLN);
@@ -20,3 +24,22 @@ static inline int sched_default_threads(){
 
 int sched_init(int nthreads, int qlen, taskfunc f, void *closure);
 int sched_spawn(taskfunc f, void *closure, struct scheduler *s);
+
+
+typedef struct Element {
+  taskfunc t;
+  void * closure;
+  Element * prec;
+} Element;
+
+typedef struct Lifo {
+  Element * dernier;
+} Lifo;
+
+void empiler(struct Lifo * lifo, taskfunc f, void *closure);
+
+Element* depiler(struct Lifo * lifo);
+
+int tailleLifo(struct Lifo * lifo);
+
+#endif
